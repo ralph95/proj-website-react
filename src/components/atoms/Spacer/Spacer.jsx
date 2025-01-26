2df64f85f041ebbb47da01c8597d8ef8c136a079
ralph95 <ralphbaylon95@gmail.com>
2025-01-26 16:13:51 +0800

    Updated Spacer to accept colors style

--- src/components/atoms/Spacer/Spacer.jsx
+++ src/components/atoms/Spacer/Spacer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Spacer = ({ height = "2rem" }) => {
-  return <div style={{ height }}></div>;
+const Spacer = ({ height = "2rem", style = {} }) => {
+  return <div style={{ height, ...style }}></div>; // Apply style prop here
 };
+
 export default Spacer;
