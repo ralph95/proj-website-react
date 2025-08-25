abf7b6f97cfac9af0ef5bcb192e69d296e8da966
ralph95 <ralphbaylon95@gmail.com>
2025-08-25 22:50:07 +0800

    Button: Updated button hover color

--- src/components/atoms/PrimaryButton/PrimaryButton.jsx
+++ src/components/atoms/PrimaryButton/PrimaryButton.jsx
@@ -5,7 +5,7 @@ const PrimaryButton = ({ children, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="bg-[#F26522] text-[#0D0D0D] text-base sm:text-lg font-extrabold px-4 sm:px-6 py-2 sm:py-2.5 rounded-md hover:bg-[#e65c1f] focus:outline-none focus:ring-2 focus:ring-[#F26522]"
+      className="bg-[#F26522]  text-[#0D0D0D] text-base sm:text-lg font-extrabold px-4 sm:px-6 py-2 sm:py-2.5 rounded-md hover:bg-[#845BB3] focus:outline-none focus:ring-2 focus:ring-[#F26522]"
     >
       {children}
     </button>
