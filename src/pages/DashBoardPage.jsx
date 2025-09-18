8dc534493f9c8597ddc3bd7da259ec9097c028c7
ralph95 <ralphbaylon95@gmail.com>
2025-09-19 01:26:23 +0800

    DarkMode: Update styling

--- src/pages/DashBoardPage.jsx
+++ src/pages/DashBoardPage.jsx
@@ -118,7 +118,7 @@ export default function AdminDashboard() {
                     onExport={handleExport}
                   />
                   <SystemStatus />
-                  <RecentActivity />
+                  {/* <RecentActivity /> */}
                 </div>
               </div>
             </div>
