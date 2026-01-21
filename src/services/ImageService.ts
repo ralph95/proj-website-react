d5e1462a1350cb792056bd7f359b76b487b6b4b3
ralph95 <ralphbaylon95@gmail.com>
2026-01-21 22:16:15 +0800

    ImageStack: Updated code to fetch images and added axiom package

---
+++ src/services/ImageService.ts
@@ -0,0 +1,20 @@
+// src/services/ImageService.ts
+import axios from "axios";
+
+export interface ImageItem {
+  name: string;
+  s3_key: string;
+  url: string;
+}
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const fetchImages = async (): Promise<ImageItem[]> => {
+  try {
+    const res = await axios.get(`${API_URL}/images`);
+    return res.data;
+  } catch (err) {
+    console.error("Failed to fetch images:", err);
+    return []; // Return empty array on error
+  }
+};
