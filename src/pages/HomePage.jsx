8ee7c16e4dd00ea3cfa4f282f7bbd6c28c51ff50
ralph95 <ralphbaylon95@gmail.com>
2026-01-28 22:05:07 +0800

    RotatingText: Implementation and not quite finish

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -32,6 +32,7 @@ import {
   SiTypescript,
   SiTailwindcss,
 } from "react-icons/si";
+import RotatingText from "@/components/RotatingText";
 
 const techLogos = [
   { node: <SiReact />, title: "React", href: "https://react.dev" },
@@ -421,6 +422,23 @@ const HomePage = () => {
         <Spacer height="2rem" style={{ backgroundColor: "#0d0d0d" }} />
         <StyledInvesedPolygon />
       </motion.div>
+      <Container centerBoth>
+        <RotatingText
+          texts={["Full Stack Developer", "Web Developer", "DevOps Engineer"]}
+          mainClassName="px-2 sm:px-2 md:px-3 bg-[#5227ff] text-white [&_*]:text-white overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+          staggerFrom={"last"}
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          exit={{ y: "-120%" }}
+          staggerDuration={0.025}
+          splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+          transition={{ type: "spring", damping: 30, stiffness: 400 }}
+          rotationInterval={2000}
+        />
+      </Container>
+      <StyledPolygon />
+      <Spacer height="2rem" style={{ backgroundColor: "#0d0d0d" }} />
+      <StyledInvesedPolygon />
       <Container>
         <Spacer />
         <Spacer />
